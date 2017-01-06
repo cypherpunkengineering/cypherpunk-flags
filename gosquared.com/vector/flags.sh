3239d79359bb29f465f3de78f456944a3af27bf0
J. Maurice <j@wiz.biz>
2017-01-06 11:42:56 +0900

    Rename country flags according to ISO 3166 codes

--- gosquared.com/vector/flags.sh
+++ gosquared.com/vector/flags.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 for i in Flag_of_*;do
 	country=`echo "$i"|sed -e 's/Flag_of_//' -e 's/\.svg//' -e 's/_/ /g' -e 's/the //'`
-	cc=`grep -i "$country" $HOME/Development/cypherpunk/cypherpunk-backend-framework/util/world.coffee|cut -d : -f1`
-	echo "$country is $cc"
+	cc=`grep -i "$country" $HOME/Development/cypherpunk/cypherpunk-database/framework/util/world.coffee|cut -d : -f1|xargs|head -1`
+	if [ ! -z "${cc}" ];then
+		echo "$country is $cc"
+		cp "$i" "flag-${cc}.svg"
+	fi
 done
